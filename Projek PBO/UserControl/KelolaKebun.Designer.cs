a0c117300470f6a0da8ef756769e438a773e794b
fathh <fath.halmar@gmail.com>
2026-06-19 01:35:26 +0700

    s

--- Projek PBO/UserControl/KelolaKebun.Designer.cs
+++ Projek PBO/UserControl/KelolaKebun.Designer.cs
@@ -111,6 +111,7 @@
             // 
             // dataGridView1
             // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(16, 315);
             dataGridView1.Name = "dataGridView1";
