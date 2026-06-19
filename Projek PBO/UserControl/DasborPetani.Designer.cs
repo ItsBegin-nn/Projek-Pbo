449dca13fe0b5b61ea2a78b88288e01735021ec6
Aditya Chandra <Aditya Chandra@MSI>
2026-06-19 21:25:26 +0700

    a

--- Projek PBO/UserControl/DasborPetani.Designer.cs
+++ Projek PBO/UserControl/DasborPetani.Designer.cs
@@ -13,15 +13,75 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DasborPetani));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(95, 131);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 41);
+            label1.TabIndex = 2;
+            label1.Text = "5";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(331, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 41);
+            label2.TabIndex = 3;
+            label2.Text = "5";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(537, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 41);
+            label3.TabIndex = 4;
+            label3.Text = "5";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(96, 293);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(604, 158);
+            dataGridView1.TabIndex = 5;
             // 
             // DasborPetani
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "DasborPetani";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private DataGridView dataGridView1;
     }
 }
