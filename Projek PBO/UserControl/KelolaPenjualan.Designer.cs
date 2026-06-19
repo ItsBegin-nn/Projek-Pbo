d31bb6ea61433233fa604c3514b5a3d6c994ff34
gyunivers-beep <naysilaamelia06@gmail.com>
2026-06-19 08:20:15 +0700

    fitur kelola penjualan

--- Projek PBO/UserControl/KelolaPenjualan.Designer.cs
+++ Projek PBO/UserControl/KelolaPenjualan.Designer.cs
@@ -13,12 +13,192 @@
 
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 26F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "KelolaPenjualan";
-            this.Size = new System.Drawing.Size(884, 685);
-            this.ResumeLayout(false);
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label6 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(67, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(198, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Kelola Penjualan";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(108, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Pilih Hasil Panen";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(645, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 24);
+            label3.TabIndex = 2;
+            label3.Text = "Nama Pembeli";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(108, 200);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 24);
+            label4.TabIndex = 3;
+            label4.Text = "Tanggal Penjualan";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(645, 200);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Harga Jual";
+            label5.Click += label5_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(108, 227);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(645, 117);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 31);
+            textBox1.TabIndex = 6;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(108, 117);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(300, 33);
+            comboBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(645, 227);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 31);
+            textBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(645, 288);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 9;
+            button1.Text = "Simpan";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(833, 288);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 10;
+            button2.Text = "Hapus";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Palatino Linotype", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(108, 338);
+            label6.Name = "label6";
+            label6.Size = new Size(169, 24);
+            label6.TabIndex = 11;
+            label6.Text = "Riwayat Penjualan";
+            label6.Click += label6_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(108, 365);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(837, 164);
+            dataGridView1.TabIndex = 12;
+            // 
+            // KelolaPenjualan
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(dataGridView1);
+            Controls.Add(label6);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "KelolaPenjualan";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label6;
+        private DataGridView dataGridView1;
     }
 }
--- Projek PBO/UserControl/KelolaPenjualan.cs
+++ Projek PBO/UserControl/KelolaPenjualan.cs
@@ -1,13 +1,198 @@
-﻿namespace Projek_PBO.Views
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Projek_PBO.Views
 {
     public partial class KelolaPenjualan : BaseUserControl
     {
+        private readonly string connString =
+            "Host=localhost;Port=5432;Database=Projek PBO;Username=postgres;Password=Nay!130408";
+
+        private int idPenjualanTerpilih = 0;
+
         public KelolaPenjualan(string namaPengguna) : base(namaPengguna)
         {
             InitializeComponent();
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.CellClick += dataGridView1_CellClick;
         }
 
-        public override void MuatData() { }
         public override string GetJudulForm() => "Kelola Penjualan";
+
+        public override void MuatData()
+        {
+            try
+            {
+                using var conn = new NpgsqlConnection(connString);
+                conn.Open();
+                var da = new NpgsqlDataAdapter(
+                    @"SELECT id_panen,
+                             'Panen #' || id_panen || ' - ' ||
+                             TO_CHAR(tanggal_panen, 'DD-MM-YYYY') ||
+                             ' (' || berat_kg || ' kg)' AS label_panen
+                      FROM panen ORDER BY tanggal_panen DESC", conn);
+                var dt = new DataTable();
+                da.Fill(dt);
+                comboBox1.DataSource = dt;
+                comboBox1.DisplayMember = "label_panen";
+                comboBox1.ValueMember = "id_panen";
+                comboBox1.SelectedIndex = -1;
+            }
+            catch (Exception ex) { MessageBox.Show("Gagal memuat panen: " + ex.Message); }
+
+            try
+            {
+                using var conn = new NpgsqlConnection(connString);
+                conn.Open();
+                var da = new NpgsqlDataAdapter(
+                    @"SELECT id_penjualan, id_panen, tanggal_penjualan,
+                             nama_pembeli, harga_jual, total_pendapatan
+                      FROM penjualan ORDER BY tanggal_penjualan DESC", conn);
+                var dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex) { MessageBox.Show("Gagal memuat riwayat: " + ex.Message); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1 ||
+                string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Lengkapi semua data dulu.");
+                return;
+            }
+
+            if (!decimal.TryParse(textBox2.Text, out decimal hargaPerKg))
+            {
+                MessageBox.Show("Harga jual harus angka.");
+                return;
+            }
+
+            int idPanen = Convert.ToInt32(comboBox1.SelectedValue);
+
+            try
+            {
+                using var conn = new NpgsqlConnection(connString);
+                conn.Open();
+
+                var cmdKg = new NpgsqlCommand(
+                    "SELECT COALESCE(berat_kg, 0) FROM panen WHERE id_panen = @id", conn);
+                cmdKg.Parameters.AddWithValue("@id", idPanen);
+                decimal jumlahKg = Convert.ToDecimal(cmdKg.ExecuteScalar());
+                decimal total = hargaPerKg * jumlahKg;
+
+                if (idPenjualanTerpilih == 0)
+                {
+                    var cmd = new NpgsqlCommand(
+                        @"INSERT INTO penjualan
+                          (id_panen, tanggal_penjualan, nama_pembeli, harga_jual, total_pendapatan)
+                          VALUES (@panen, @tgl, @pembeli, @harga, @total)", conn);
+                    cmd.Parameters.AddWithValue("@panen", idPanen);
+                    cmd.Parameters.AddWithValue("@tgl", DateOnly.FromDateTime(dateTimePicker1.Value));
+                    cmd.Parameters.AddWithValue("@pembeli", textBox1.Text.Trim());
+                    cmd.Parameters.AddWithValue("@harga", hargaPerKg);
+                    cmd.Parameters.AddWithValue("@total", total);
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    var cmd = new NpgsqlCommand(
+                        @"UPDATE penjualan SET
+                            id_panen = @panen,
+                            tanggal_penjualan = @tgl,
+                            nama_pembeli = @pembeli,
+                            harga_jual = @harga,
+                            total_pendapatan = @total
+                          WHERE id_penjualan = @id", conn);
+                    cmd.Parameters.AddWithValue("@panen", idPanen);
+                    cmd.Parameters.AddWithValue("@tgl", DateOnly.FromDateTime(dateTimePicker1.Value));
+                    cmd.Parameters.AddWithValue("@pembeli", textBox1.Text.Trim());
+                    cmd.Parameters.AddWithValue("@harga", hargaPerKg);
+                    cmd.Parameters.AddWithValue("@total", total);
+                    cmd.Parameters.AddWithValue("@id", idPenjualanTerpilih);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show($"Data tersimpan. Total pendapatan: {total:N2}");
+                MuatData();
+                ResetForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menyimpan: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Pilih data yang mau dihapus dulu.");
+                return;
+            }
+
+            int idPenjualan = Convert.ToInt32(dataGridView1.CurrentRow.Cells["id_penjualan"].Value);
+
+            if (MessageBox.Show("Yakin hapus data ini?", "Konfirmasi",
+                MessageBoxButtons.YesNo) != DialogResult.Yes) return;
+
+            try
+            {
+                using var conn = new NpgsqlConnection(connString);
+                conn.Open();
+                var cmd = new NpgsqlCommand(
+                    "DELETE FROM penjualan WHERE id_penjualan = @id", conn);
+                cmd.Parameters.AddWithValue("@id", idPenjualan);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Data dihapus.");
+                MuatData();
+                ResetForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menghapus: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            var row = dataGridView1.Rows[e.RowIndex];
+            idPenjualanTerpilih = Convert.ToInt32(row.Cells["id_penjualan"].Value);
+            comboBox1.SelectedValue = row.Cells["id_panen"].Value;
+            textBox1.Text = row.Cells["nama_pembeli"].Value?.ToString();
+            textBox2.Text = row.Cells["harga_jual"].Value?.ToString();
+
+            if (row.Cells["tanggal_penjualan"].Value != DBNull.Value)
+            {
+                var nilaiTgl = row.Cells["tanggal_penjualan"].Value;
+                dateTimePicker1.Value = (nilaiTgl is DateOnly d)
+                    ? d.ToDateTime(TimeOnly.MinValue)
+                    : Convert.ToDateTime(nilaiTgl);
+            }
+        }
+
+        private void ResetForm()
+        {
+            idPenjualanTerpilih = 0;
+            comboBox1.SelectedIndex = -1;
+            textBox1.Clear();
+            textBox2.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void label3_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void label5_Click(object sender, EventArgs e) { }
+        private void label6_Click(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
     }
 }
