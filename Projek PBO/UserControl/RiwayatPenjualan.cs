431b1462854317209b52180bc79eab70f470c675
gyunivers-beep <naysilaamelia06@gmail.com>
2026-06-20 19:03:48 +0700

    riwayat penjualan

--- Projek PBO/UserControl/RiwayatPenjualan.cs
+++ Projek PBO/UserControl/RiwayatPenjualan.cs
@@ -1,12 +1,33 @@
-﻿namespace Projek_PBO.Views
+﻿using System;
+using System.Windows.Forms;
+using Projek_PBO.Controllers;
+
+namespace Projek_PBO.Views
 {
     public partial class RiwayatPenjualan : BaseUserControl
     {
+        RiwayatPenjualanController controller = new RiwayatPenjualanController();
+
         public RiwayatPenjualan(string namaPengguna) : base(namaPengguna)
         {
             InitializeComponent();
         }
-        public override void MuatData() { }
+
+        public override void MuatData()
+        {
+            try
+            {
+                dataGridView1.DataSource = controller.GetRiwayat();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal memuat riwayat penjualan: " + ex.Message);
+            }
+        }
+
         public override string GetJudulForm() => "Riwayat Penjualan";
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
     }
 }
